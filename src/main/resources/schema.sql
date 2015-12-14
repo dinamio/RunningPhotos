e1394fa5372d6655fd25380d9d5ba2f5acd6bdc3
darthoo <ilai2006@ukr.net>
2015-12-14 22:33:00 +0200

    [issue 2] Added UserDao and UserDaoTest (n.Podlesny)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,12 @@
 CREATE TABLE Role (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255));
+create table User(
+id int primary key auto_increment,
+Login varchar(50),
+Mail varchar(50),
+Name varchar(20),
+Surname varchar(30),
+City varchar (20),
+Birthdate date,
+Role_id int,
+foreign key (Role_id) references Role(id)
+);
