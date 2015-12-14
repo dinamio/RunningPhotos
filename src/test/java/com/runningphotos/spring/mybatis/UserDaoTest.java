ae1ea8420caabb4604b2496d7f080fc2d7030cd2
Jimmy <dinamio@ukr.net>
2015-12-14 23:32:53 +0200

    [RP-2] Refactor UserDaoTest (e.godun)

--- src/test/java/com/runningphotos/spring/mybatis/UserDaoTest.java
+++ src/test/java/com/runningphotos/spring/mybatis/UserDaoTest.java
@@ -8,6 +8,7 @@ import com.runningphotos.bom.Role;
 import com.runningphotos.bom.User;
 import com.runningphotos.dao.RoleDao;
 import com.runningphotos.dao.UserDao;
+import com.runningphotos.testdata.TestData;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
@@ -16,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -25,35 +27,40 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/spring-config.xml")
-public class UserDaoTest {
+public class UserDaoTest extends TestData {
 
     private static Log log = LogFactory.getLog(RoleDaoTest.class);
 
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    private RoleDao roleDao;
+
     @Test
     public void testInsertRole(){
         log.info("testing insert User()...");
-
         User user = new User();
-
-        User anotherUser = new User();
-
-        user.setId(1);
-        anotherUser.setId(2);
-        user.setName("Runner");
-        user.setSurname("Runnerov");
+        user.setName(USERNAME);
+        user.setSurname(USER_SURNAME);
+        user.setLogin(USER_LOGIN);
+        user.setBirthDate(USER_BIRTHDAY);
+        user.setCity(USER_CITY);
+        user.setMail(USER_MAIL);
+        Role userRole = roleDao.selectAllRoles().get(0);
+        user.setRole(userRole);
+        assertEquals(0,userDao.selectAllUsers().size());
         userDao.insert(user);
-        user.setName("updut");
-        userDao.update(user);
-        anotherUser.setName("anyAnotherUser");
-        anotherUser.setSurname("anyAnotherUserSurname");
-        userDao.insert(anotherUser);
-        userDao.delete(anotherUser);
         List<User> users = userDao.selectAllUsers();
-        assertEquals(users.get(0).getName(),"updut");
-        assertEquals(users.size(),1);
+        assertEquals(1,users.size());
+        user = users.get(0);
+        assertEquals(USERNAME,user.getName());
+        assertEquals(USER_SURNAME,user.getSurname());
+        assertEquals(USER_LOGIN,user.getLogin());
+        assertEquals(dateFormat.format(USER_BIRTHDAY),dateFormat.format(user.getBirthDate()));
+        assertEquals(USER_CITY,user.getCity());
+        assertEquals(USER_MAIL,user.getMail());
+        assertEquals(userRole.getId(),user.getRole().getId());
         log.info(users);
     }
 }
---
+++ src/test/java/com/runningphotos/testdata/TestData.java
@@ -0,0 +1,27 @@
+package com.runningphotos.testdata;
+
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Jimmy on 14.12.2015.
+ */
+public class TestData {
+
+    public static final String USERNAME = "Ivan";
+
+    public static final String USER_SURNAME = "Ivanov";
+
+    public static final String USER_LOGIN = "ivani";
+
+    public static final String USER_CITY = "Kharkiv";
+
+    public static final DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+
+    public static final Date USER_BIRTHDAY = new Date();
+
+    public static final String USER_MAIL = "ivani@ukr.net";
+
+}
