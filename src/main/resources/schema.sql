16ef18714a7cf94efc028329929e07657bfb14ef
darthoo <ilai2006@ukr.net>
2015-12-21 13:15:37 +0200

    [rp 5]Add schema.sql(n.podlesny)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,7 +13,7 @@ create table User(
 id int primary key auto_increment,
 Login varchar(50),
 Mail varchar(50),
-Name varchar(20),
+Name varchar(30),
 Surname varchar(30),
 City varchar (20),
 Birthdate date,
